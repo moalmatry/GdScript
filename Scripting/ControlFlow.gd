95fdebf46e8917d826e6c17b0ef8dc4ace974a10
Mohamed Almatry <moalmatry2000@gmail.com>
2026-01-20 08:41:53 +0200

    feature: operators lesson

--- Scripting/ControlFlow.gd
+++ Scripting/ControlFlow.gd
@@ -6,5 +6,28 @@ func _run() -> void:
 	var ring_bearer := "Boromir"
 	if ring_bearer == "Boromir":
 		print("The ring bearer is Boromir")
+	elif ring_bearer == "Frodo":
+		print("The ring bearer is Frodo")
 	else:
 		print("The ring bearer is not Boromir")
+
+	var arrows := 10
+	while arrows > 0:
+		arrows -= 1
+		print("Shooting an arrow. Arrows left: %d" % arrows)
+		if arrows == 4:
+			print("Halfway there!")
+			break
+	var fellowship: Array[String] = [
+		"Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli", "Boromir"
+	]
+
+	for member in fellowship:
+		if member == "Merry":
+			break
+		elif member == "Gandalf":
+			continue
+		print("%s is on the quest." % member)
+
+	for orc in range(1, 22):
+		print("Orc %d has been defeated!" % orc)
