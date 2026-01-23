a2ef245a3d3cb47e5b7793c3903cd13f5c16c4af
Mohamed Almatry <moalmatry2000@gmail.com>
2026-01-23 17:32:10 +0200

    feature: string formatting

--- Scripting/StringFormating.gd
+++ Scripting/StringFormating.gd
@@ -3,4 +3,14 @@ extends EditorScript
 
 
 func _run() -> void:
-	pass
+	var text := "Mohamed"
+
+	print(text.length())
+	print(text[0])
+	print(text.substr(2))
+
+	# var prophecy := "The heir of Islidur shall return."
+	# var found := prophecy.find("ssssss")
+	# print(found)
+	print("Hello %s" % text)
+	print("Hello %s %s" % [text, text])
